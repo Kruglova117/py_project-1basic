fbd3635fdd4b569e34962ecf6bb39387922b7fe5
Pashkivska117 <kruglova_katerina>
2023-12-08 21:22:05 +0200

    lab ex 2 done

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print('Nothing')
-print('will work')
-print('unless you do')
+print('"Anyone who')
+print('stops')
+print('learning is old')
+print('whether at twenty or eighty"')
+
+print('Henry Ford')
